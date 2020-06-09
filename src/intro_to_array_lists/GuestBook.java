0b4a814cabbda08c2fc3cf79f53a2afaf311887a
Asherb123 <46113623+Asherb123@users.noreply.github.com>
2020-06-08 18:15:16 -0700

    Yay

--- src/intro_to_array_lists/GuestBook.java
+++ src/intro_to_array_lists/GuestBook.java
@@ -1,7 +1,36 @@
 package intro_to_array_lists;
 
-public class GuestBook {
-	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements MouseListener {
+	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names".
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton add = new JButton("Add name");
+	JButton view = new JButton("View names");
+	
+	ArrayList<String> names = new ArrayList<String>();
+	
+
+	void Gui() {
+		frame.add(panel);
+		panel.add(add);
+		panel.add(view);
+		
+		add.addMouseListener(this);
+		view.addMouseListener(this);
+		
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+		frame.pack();
+	}
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
 	// all the names added to the list. Format the list as follows:
@@ -9,4 +38,54 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource()==add) {
+		String a = JOptionPane.showInputDialog("What name would you like to add to the list?");
+		
+		names.add(a);
+		
+		
+		}
+		if (e.getSource()== view) {
+			String z= "";
+			
+			for (int i = 0; i < names.size(); i++) {
+				z+= "Guest #" + (i+1) + ": " +names.get(i) + "\n";
+			}
+			JOptionPane.showMessageDialog(null, z);
+		}
+	
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	public static void main(String[] args) {
+		GuestBook c = new GuestBook();
+		c.Gui();
+	}
+
 }
